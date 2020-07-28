19e7426486905d803a8190a794053522deffdfaa
Paulo Henrique Santos da Silva <phenrique@secsaude.corp>
2020-07-28 14:37:40 -0300

    ...

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,21 +1,74 @@
 'use strict'
 
+const Database = use('Database')
+const User = use('App/Models/User')
+const Role = user('Role')
 class AuthController {
+  async register({ request, response }) {
+    const trx = await Database.beginTransaction()
 
-  async register({ request, response }){}
+    try {
+      const data = request.only(['name', 'surname', 'email', 'password'])
 
-  async login({ request, response, auth }){}
+      const user = await User.create({ data }, trx)
 
-  async refresh({ request, response, auth }){}
+      const userRole = await Role.findBy('slug', 'client')
 
-  async logout({ requests, response, auth }){}
+      await user.roles().attach([userRole.id], null, trx) //attach() sempre espera um array
 
-  async forgot({ request, response }){}
+      await trx.commit()
 
-  async remember({ request, response }){}
+      return response.status(201).send({ data: user })
+    } catch (error) {
+      await trx.rollback()
 
-  async reset({ request, response }){}
+      return response.status(400).send({
+        message: 'Erro ao realizar cadastro!',
+      })
+    }
+  }
 
+  async login({ request, response, auth }) {
+    const { email, password } = request.all()
+
+    let data = await auth.withRefreshToken().attempt(email, password)
+
+    return response.send({ data })
+  }
+
+  async refresh({ request, response, auth }) {
+    let refresh_token = request.input('refresh_token')
+
+    if (!refresh_token) {
+      refresh_token = request.header('refresh_token')
+    }
+
+    const user = await auth
+      .newRefreshToken()
+      .generateForRefreshToken(refresh_token)
+
+    return response.send({ data: user })
+  }
+
+  async logout({ requests, response, auth }) {
+    let refresh_token = requests.input('refresh_token')
+
+    if (!refresh_token) {
+      refresh_token = requests.header('refresh_token')
+    }
+
+    const loggedOut = await auth
+      .athenticator('jwt')
+      .revokeTokens([refresh_token], true)
+
+    return response.status(204).send({})
+  }
+
+  async forgot({ request, response }) {}
+
+  async remember({ request, response }) {}
+
+  async reset({ request, response }) {}
 }
 
 module.exports = AuthController
